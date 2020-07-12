c1ab4c4754f2476c6afe65275d5a2a5207e2dde6
JMorales <josephzx1@hotmail.com>
2020-07-11 23:33:13 -0600

    Se agrega menu amburguesa y los eventos de JS

---
+++ js/event.js
@@ -0,0 +1,30 @@
+// 'use strict'
+   var menu=document.querySelector(".menu");
+   var burgerButton=document.querySelector("#burger-menu");
+   var screen=window.matchMedia('screen and (max-width: 767px)');
+    
+
+    screen.addListener(validation)
+    function validation(event){
+        console.log(event)
+        if(event.matches)
+        {
+
+            burgerButton.addEventListener('click', hideShow)
+        }else{
+            burgerButton.removeEventListener('click', hideShow)
+
+        }
+    }
+
+   
+    function hideShow(){
+        if(menu.classList.contains('isActive'))
+       {
+        menu.classList.remove('isActive');
+
+       }else{
+        menu.classList.add('isActive');
+
+       }
+    }
